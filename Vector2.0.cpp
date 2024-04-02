9f7c30fe63ed9b5f91f496bd5f69a25c8b6dd7a8
CatLover01 <160437882+CatLover01@users.noreply.github.com>
2024-04-01 22:25:49 -0400

    Vector2.0.cpp

---
+++ Vector2.0.cpp
@@ -0,0 +1,104 @@
+// This is my Custom Implementation of a Vector Container!!!
+// Keep in mind that I am still a beginner :3
+#include <iostream>
+#include <memory>  
+#include <initializer_list>
+#include <type_traits> // for std::is_trivially_copyable_v
+#include <cstring> // for std::memcpy
+
+// This Macro is used when no pointer exist (i.e. when you just cleared the Pector)
+#define newCapacity 5
+
+template<typename T>
+class Pector {
+private:
+	int m_size;
+	int m_capacity;
+	std::unique_ptr<T[]>m_ptr;
+	bool isReset{false};
+
+public:
+	Pector(int capacity) : m_capacity{ static_cast<int>(capacity + (capacity * 0.75)) },
+		m_size{ capacity }, m_ptr{ std::make_unique<T[]>(m_capacity) } {}
+	Pector(std::initializer_list<T> list) : m_capacity{ static_cast<int>(list.size() + (list.size() * 0.75)) },
+		m_size{ static_cast<int>(list.size()) }, m_ptr{ std::make_unique<T[]>(m_capacity) } {
+		int num{ 0 };
+		for (auto it{ list.begin() }; it != list.end(); it++) {
+			m_ptr[num] = *it;
+			num++;
+		}
+	}
+	void print() {
+		for (int i{}; i < m_size; i++) {
+			std::cout << *(m_ptr.get() + i) << '\n';
+		}
+	} 
+	T& operator[](int index) {
+		if (index < m_size) {
+			return *(m_ptr.get() + index);
+		}
+		throw std::out_of_range("Access is Out of Bounds");
+	}
+	void pop_back() {
+		if (m_size > 0) {
+			*(m_ptr.get() + (m_size - 1)) = 0;
+			m_size--;
+		}
+	}
+	void push_back(T value) {
+		if (isReset) {
+			m_ptr = std::make_unique<T[]>(newCapacity); // No magic number so a Macro...
+			isReset = false;
+		}
+		if (m_size >= m_capacity) {
+			m_size++;
+			m_capacity = static_cast<int>(m_size + (m_size * 0.75)); // Increasing the Array Capacity Geometrically
+			std::unique_ptr<T[]>tempPtr{ std::make_unique<T[]>(m_capacity) };
+
+			if (std::is_trivially_copyable_v<T> == true) {
+				// Faster
+				std::memcpy(tempPtr.get(), m_ptr.get(), sizeof m_ptr);
+				*(tempPtr.get() + (m_size - 1)) = value;
+			}
+			else {
+				// Slower
+				for (int i{}; i < m_capacity; i++) {
+					if (i == m_capacity - 1) {
+						*(tempPtr.get() + i) = value;
+					}
+					else {
+						*(tempPtr.get() + i) = *(m_ptr.get() + i);
+					}
+				}
+			}
+			// m_ptr now points to tempPtr
+			m_ptr.swap(tempPtr);
+			tempPtr.reset();
+		}
+		else {
+			m_size++;
+			m_ptr[m_size - 1] = value;
+		}
+	}
+	int size() { return m_size; }
+	bool empty() { return m_size <= 0; }
+	int capacity() { return m_capacity; }
+	void clear() {
+		m_size = 0;
+		m_capacity = 0;
+		m_ptr.reset();
+		isReset = true;
+	}
+	T* begin() { return m_ptr.get(); } // Iterators
+	T* end() { return m_ptr.get() + m_size; }
+	const T* begin() const { return m_ptr.get(); }
+	const T* end() const { return m_ptr.get() + m_size; }
+	const T* cbegin() const { return m_ptr.get(); }
+	const T* cend() const { return m_ptr.get() + m_size; }
+	std::reverse_iterator<T*> rbegin() { return std::reverse_iterator<T*>(m_ptr.get() + m_size); }
+	std::reverse_iterator<T*> rend() { return std::reverse_iterator<T*>(m_ptr.get()); }
+	const std::reverse_iterator<T*> rbegin() const { return std::reverse_iterator<T*>(m_ptr.get() + m_size); }
+	const std::reverse_iterator<T*> rend() const { return std::reverse_iterator<T*>(m_ptr.get()); }
+	const std::reverse_iterator<const T*> crbegin() const { return std::reverse_iterator<const T*>(m_ptr.get() + m_size); }
+	const std::reverse_iterator<const T*> crend() const { return std::reverse_iterator<const T*>(m_ptr.get()); }
+};
